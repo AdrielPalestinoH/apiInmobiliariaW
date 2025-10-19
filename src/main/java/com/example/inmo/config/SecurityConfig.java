d0046141729f6dc56dfbac18f38e439f7d016443
Adriel <isc.adrielpalestino@gmail.com>
2025-10-19 15:43:22 -0600

    fix cors global 11

--- src/main/java/com/example/inmo/config/SecurityConfig.java
+++ src/main/java/com/example/inmo/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://calm-rock-0b41dac0f.1.azurestaticapps.net"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
